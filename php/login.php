362b0e63a297ebac28e4635c9ce1a73d706c4a28
Lotacodic <Ngenejoshua05@gmail.com>
2022-06-03 15:04:13 +0100

    my commits

---
+++ php/login.php
@@ -0,0 +1,19 @@
+<?php
+if(isset($_POST['submit'])){
+    $username = 
+    $password = 
+
+loginUser($email, $password);
+
+}
+  /*
+    This function checks if username and password
+    from file match that which is passed from the form
+ */
+
+function loginUser($email, $password){
+ 
+}
+
+echo "HANDLE THIS PAGE";
+
